0355934153340d9638e129fa3be725a39744811d
UjeshChak <ujesh.chakravarty@jpschools.org>
2026-03-05 11:22:52 -0600

    Update VirtualPet.java

--- VirtualPet.java
+++ VirtualPet.java
@@ -1,3 +1,73 @@
 public class VirtualPet {
+    private String name;
+    private int energy;
+    private int weight;
+    private int happiness;
+    private int ageMonths;
+    private int ageYears;
     
+    public VirtualPet(String name) {
+      this.name = name;
+      this.energy = 0;
+      this.weight = 5;
+      this.happiness = 0;
+      this.ageMonths = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return name + "'s information: \nEnergy: " + energy +
+"\nWeight: " + weight + "\nHappiness: "+ happiness + "\nAge: " +
+ageYears + " years and " + ageMonths + " months";
+    }
+
+    public int getEnergyLevel() {
+        return energy;
+    }
+
+    public int getHappinessLevel() {
+        return happiness;
+    }
+    public int getAgeMonths() {
+      return ageMonths;
+    }
+    public int getWeight() {
+      return weight;
+    }
+
+    public void feed(Food food) {
+        this.energy += food.getEnergyIncrease();
+        this.weight += food.getWeightGain();
+        this.happiness += food.getHappinessIncrease();
+    }
+
+    public void play(Game game) {
+        if (game.isWinner()) {
+            this.happiness += game.getHappinessIncr();
+            this.weight -= game.getWeightDecr();
+        }
+    }
+
+    public void updateStatus() {
+        if (happiness > 0) {
+            happiness--;
+        }
+
+        if (energy > 0) {
+            energy--;
+        }
+
+        ageMonths++;
+        ageConverter();
+    }
+
+    private void ageConverter() {
+        if (ageMonths >= 12) {
+            ageYears++;
+            ageMonths = 0;
+        }
+    }
 }
